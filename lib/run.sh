63ff5a6d364bab4c396c056b997b8ad33b2d8337
Dan Moore <grep.daniel@gmail.com>
2024-10-13 23:27:48 -0400

    Create run.sh

---
+++ lib/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#
+# --------------------------------------------------
+# Appends publish dates to '/content/**/*.mdx' files
+# --------------------------------------------------
+# python frontmatter.py
+# --------------------------------------------------
+
+# Running dev
+(cd .. && exec npm run dev)
